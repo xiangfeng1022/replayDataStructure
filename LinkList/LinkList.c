a6fd3bc921cc5827d46d804f1a3328ef497f1096
xiangfeng1022 <1365725301@qq.com>
2023-12-20 15:04:17 +0800

    [修改]完善指针的释放API接口(销毁)

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -194,12 +194,29 @@ int LinkListGetLength(LinkList *pList, int *pSize)
     {
         *pSize = pList->len;
     }
-    return ret;
+    /* 返回链表的长度 */
+    return pList->len;
 }
 
 /* 链表的销毁 */
 int LinkListDestroy(LinkList *pList)
-{
+{    
+    int ret = 0;
+    /* 我们使用头删释放链表 */
+    int size = 0;
+    while (LinkListGetLength(pList, &size))
+    {
+        LinkListHeadDel(pList);
+    }
+
+    if (pList->head != NULL)
+    {
+        free(pList->head);
+        /* 指针置为NULL */
+        pList->head == NULL;
+        pList->tail = NULL;
+    }
+    return ret;
 }
 
 /* 链表遍历接口 */
