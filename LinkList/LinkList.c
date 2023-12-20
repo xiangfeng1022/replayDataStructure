6e127f286cd31e25f14460fe3f77f412c0063284
xiangfeng1022 <1365725301@qq.com>
2023-12-20 10:28:01 +0800

    [修改]完成链表的初始化API接口

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -1,9 +1,44 @@
 #include "LinkList.h"
+#include <stdlib.h>
+#include <string.h>
+
+
+enum STATUS_CODE
+{
+   ON_SUCCESS,
+   NULL_PTR,
+   MALLOC_ERROR,
+   INVALID_ACCESS,
+
+};
 
 /* 链表初始化 */
 int LinkListInit(LinkList **pList)
-{
+{   
+    int ret = 0;
+    LinkList *list = (LinkList *)malloc(sizeof(LinkList) * 1);
+    if (list == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    memset(list, 0, sizeof(LinkList) * 1);
+    
+    list->head = (LinkNode *)malloc(sizeof(LinkNode) * 1);
+    if (list->head == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    memset(list->head, 0, sizeof(LinkNode) * 1);
+    list->head->data = 0;
+    list->head->next = NULL;
+    
+    /* 链表的长度为0 */
+    list->len = 0;
 
+    /* 二级指针 */
+    *pList = list;
+
+    return ret;
 }
 
 /* 链表头插 */
@@ -63,5 +98,5 @@ int LinkListDestroy(LinkList * pList)
 /* 链表遍历接口 */
 int LinkListForeach(LinkList * pList)
 {
-    
+
 }
