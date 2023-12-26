c797146d2a71e54d36e3ef138ccfe26be24f6286
xiangfeng1022 <1365725301@qq.com>
2023-12-26 21:35:29 +0800

    [修改]完成二叉搜索树的插入结点的API接口

--- BinarySearchTree/binarySearchTree.c
+++ BinarySearchTree/binarySearchTree.c
@@ -52,10 +52,82 @@ int binarySearchTreeInit(BinarySearchTree **pBstree)
     return ret;
 }
 
+static int compareFunc(ELEMENTTYPE val1, ELEMENTTYPE val2)
+{
+    return val1 - val2;
+}
 /* 二叉搜索树的插入 */
-int binarySearchTreeInsert(BinarySearchTree **pBstree, ELEMENTTYPE val)
+int binarySearchTreeInsert(BinarySearchTree *pBstree, ELEMENTTYPE val)
 {
     int ret = 0;
+    /* 空树 */
+    if (pBstree->size == 0)
+    {
+        /* 更新树的结点 */
+        (pBstree->size)++;
 
+        pBstree->root->data = val;
+        return ret;
+    }
+    
+    /* travelNode 指向根结点 */
+    BSTreeNode * travelNode = pBstree->root;
+
+    BSTreeNode * parentNode = pBstree->root;
+
+    /* 确定符号 */
+    int cmp = 0;
+    while (travelNode != NULL)
+    {   
+        /* 标记父结点 */
+        parentNode = travelNode;
+        cmp = compareFunc(val, travelNode->data);
+        /* 插入元素 < 遍历到的结点 */
+        if (cmp < 0)
+        {
+            travelNode = travelNode->left;
+        }
+        else if (cmp > 0)
+        {
+            travelNode = travelNode->right;
+        }
+        else
+        {   
+            /* 插入元素 = 遍历到的结点 */
+            return ret;
+        }
+    }
+    
+    /* 分配新结点 */
+    BSTreeNode * newBstNode = (BSTreeNode *)malloc(sizeof(BSTreeNode) * 1);
+    if (newBstNode == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    memset(newBstNode, 0, sizeof(BSTreeNode) * 1);
+    
+    {
+        newBstNode->data = 0;
+        newBstNode->left = NULL;
+        newBstNode->right = NULL;
+        newBstNode->parent = NULL;
+    }
+    /* 新结点赋值 */
+    newBstNode->data = val;
+
+    /* 挂在左子树 */
+    if (cmp < 0)
+    {
+        parentNode->left = newBstNode;
+    }
+    else
+    {   
+        /* 挂在右子树 */
+        parentNode->right = newBstNode;
+    }
+    /* 新结点的parent指针赋值 */
+    newBstNode->parent = parentNode;
+    /* 更新树的结点 */
+    (pBstree->size)++;
     return ret;
 }
